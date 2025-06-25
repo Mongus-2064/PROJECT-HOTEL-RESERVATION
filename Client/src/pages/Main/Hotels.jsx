91e14ad7a98681e6a04bb6331ee248f6113f61a8
Mongus2064 <mongus2064@gmail.com>
2025-06-25 15:57:34 +0545

    Minor changes

--- Client/src/pages/Main/Hotels.jsx
+++ Client/src/pages/Main/Hotels.jsx
@@ -1,40 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "..//..//API/api.js";
 
 const Hotels = () => {
-  return (
-    <div className="bg-gradient-to-r from-gray-900 to bg-green-800 min-h-screen overflow-y-scroll p-2 grid grid-cols-4 ">
-      <div className="flex flex-col p-2 bg-gradient-to-r from-blue-400 to-gray-500 shadow-2xl rounded-md w-50 h-72">
-        <img
-          src="https://plus.unsplash.com/premium_photo-1661964071015-d97428970584?q=80&w=1020&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="hotelimage"
-          className="w-50 rounded-md mb-2"
-        />
-        <div className="flex justify-center gap-4 flex-col ">
-          <p>Himalaya hotel</p>
-          <p>Kathmandu </p>
-          <div className="flex gap-3">
-            <strong>4800 / Night</strong>
-            <button>Book Now</button>
-          </div>
-        </div>
+  const [hotels, setHotels] = useState([]);
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      try {
+        const res = await api.get("/hotel/gethotels");
+        console.log(res.data);
+        setHotels(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchdata();
+  }, []);
+  return <div>
+    {Array.isArray(hotels) && hotels?.map((h,i)=>(
+      <div>
+        <p>{h.Hotles.name}</p>
       </div>
-      <div className="flex flex-col p-2 bg-gradient-to-r from-blue-400 to-gray-500 shadow-2xl rounded-md w-50 h-72">
-        <img
-          src="https://plus.unsplash.com/premium_photo-1661964071015-d97428970584?q=80&w=1020&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="hotelimage"
-          className="w-50 rounded-md mb-2"
-        />
-        <div className="flex justify-center gap-4 flex-col ">
-          <p>Himalaya hotel</p>
-          <p>Kathmandu </p>
-          <div className="flex gap-3">
-            <strong>4800 / Night</strong>
-            <button>Book Now</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    ))}
+  </div>;
 };
 
 export default Hotels;
